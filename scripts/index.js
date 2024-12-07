9d5fada0c8679675dbf542f0f46f1a683115d969
Viktoriia Trubina <trubina.victoria@gmail.com>
2024-12-07 12:54:11 +0100

    project completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+// Получаем шаблон и список карточек из DOM
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+// Создаём карточки
+function createCard(cardData, onDelete) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция создания карточки
+  // Заполняем данные карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardImage.src = cardData.link; // Устанавливаем ссылку на изображение
+  cardImage.alt = cardData.name; // Альтернативный текст изображения
+  cardTitle.textContent = cardData.name; // Название карточки
 
-// @todo: Вывести карточки на страницу
+  // Добавляем обработчик для удаления карточки
+  deleteButton.addEventListener('click', () => {
+    onDelete(cardElement); // Вызов переданного колбэка
+  });
+  return cardElement;
+}
+// Удаление карточки
+function handleCardDelete(cardElement) {
+  cardElement.remove();
+}
+
+// Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, handleCardDelete);
+  cardList.append(cardElement);
+});
